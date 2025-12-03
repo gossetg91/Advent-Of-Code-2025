241dcc02c67b4af4a14b675d1e8ee4bd11de8e1c
Guillaume Gosset <gosset.guillaume@uqam.ca>
2025-12-03 13:31:34 -0500

    Add J3

---
+++ J3/part2.rs
@@ -0,0 +1,37 @@
+use std::fs::read_to_string;
+use std::str::FromStr;
+
+fn get_min(lower_bound : usize, upper_bound : usize, values : &Vec<i8>) -> (i8,usize){
+    let mut max = values[lower_bound];
+    let mut max_idx = lower_bound;
+    for i in (lower_bound + 1)..upper_bound{
+        let current = values[i];
+        if current > max{
+            max = current;
+            max_idx = i;
+            if current == 9 { break; }
+        }
+    }
+    return (max, max_idx)
+}
+
+fn main(){
+    let data = read_to_string("./inputs/input.txt").unwrap();
+
+    let mut sum : i64 = 0;
+    for current in data.split("\n") {
+        let values = current.chars().map(|char| i8::from_str(&char.to_string()).unwrap()).collect::<Vec<i8>>();
+        if values.len() == 0{ continue; }
+
+        let seq_length = 12;
+        let mut current_mul : i64 = 100000000000;
+        let mut current_start = 0;
+        for i in 1..=seq_length {
+            let (max, max_idx) = get_min(current_start, values.len()-(seq_length-i), &values);
+            sum += (max as i64 * current_mul) as i64;
+            current_start = max_idx+1;
+            current_mul /= 10;
+        }
+    }
+    println!("Result: {}", sum);
+}
