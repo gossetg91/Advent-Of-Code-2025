a84fb1c52a50c7bdf7d297b40e12e0eee6ce573f
Guillaume Gosset <gosset.guillaume@uqam.ca>
2025-12-04 22:35:14 -0500

    J4 improved v2

--- J4/part2.rs
+++ J4/part2.rs
@@ -1,42 +1,44 @@
 use std::fs::read_to_string;
 
 fn main() {
-    let mut map : Vec<bool> = Vec::new();
-    let inner_size;
-    let mut rows = 0;
+    let move_x = [-1,-1,-1, 0, 0, 1, 1, 1];
+    let move_y = [-1, 0, 1,-1, 1,-1, 0, 1];
 
-    for current in read_to_string("./inputs/input.txt").unwrap().split("\n") {
+    let cols;
+    let mut rows :i32 = 0;
+
+    let mut map : Vec<char> = Vec::new();
+    for current in read_to_string("./inputs/big.txt").unwrap().split("\n") {
         if current.len() == 0 {continue;}
         rows += 1;
         for current_char in current.chars() {
-            map.push(current_char == '@');
+            map.push(current_char);
         }
     }
-    inner_size = (map.len() / rows) as i32;
+    cols = map.len() as i32 / rows;
 
     let mut accessible = 0;
     let mut removed = true;
     while removed{
         removed = false;
         for i in 0..rows as i32 {
-            for j in 0..inner_size {
-                let current_index = (i * inner_size + j) as usize;
-                if !map[current_index] { continue; }
+            for j in 0..cols {
+                let current_index = (i * cols + j) as usize;
+                if map[current_index] != '@' { continue; }
 
                 let mut count = 0;
-                'outer: for ii in if i == 0 {i} else {i - 1}..=if i < rows as i32 - 1 {i + 1} else {i} {
-                    let on_verif_line = ii == i;
-                    for jj in (if j == 0 { if on_verif_line {j + 1} else {j} } else {j - 1}..=if j < inner_size - 1 {j + 1} else {j}).step_by(if on_verif_line {2} else {1}) {
-                        if map[(ii * inner_size + jj) as usize] {
-                            count += 1;
-                            if count >= 4 { break 'outer; }
-                        }
+                for k in 0..8 {
+                    let pos_x = i + move_x[k];
+                    let pos_y = j + move_y[k];
+
+                    if pos_x >= 0 && pos_x < rows && pos_y >= 0 && pos_y < cols {
+                        count += (map[(pos_x * cols + pos_y) as usize ] == '@') as i32;
                     }
                 }
 
                 if count < 4 {
                     removed = true;
-                    map[current_index] = false;
+                    map[current_index] = '.';
                     accessible += 1;
                 }
             }
